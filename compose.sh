f35cd71470f5ef45e2cffe19059b45cdf6557775
Prateek Bagrecha (Extern) <PBagrecha.extern@chip.de>
2016-12-21 14:55:13 +0100

    first commit

---
+++ compose.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+## docker login with ops user
+docker login -u prateek057 -p Bakuman057
+## pull new images
+docker-compose pull
+## stop old containers
+docker-compose stop
+## remove old containers
+docker-compose rm -f
+## start new containers
+docker-compose up -d
+##copy dump to container
+docker cp dump.sql $(docker ps --format "{{.Names}}" | grep db):/dump.sql
+##restore data base
+#docker exec -t $(docker ps --format "{{.Names}}" | grep db) /bin/bash -c "export TERM=dumb"
+docker exec -ti $(docker ps --format "{{.Names}}" | grep db) "cat dump.sql | mysql -uroot -pexample wordpress"
+exit
